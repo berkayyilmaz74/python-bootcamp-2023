913449213dfaacdad75383599de01bb9bdebb15a
berkayyilmaz74 <brkyyilmaz74@gmail.com>
2023-03-12 23:12:07 +0300

    homework1
    day one homework

---
+++ odev1.py
@@ -0,0 +1,66 @@
+#Soru 1
+#Veri Tipleri
+
+#Veri tipi str =>Örnek
+x = "Berkay"
+
+#Veri tipi int =>Örnek
+x = 20
+
+#Veri tipi float =>Örnek
+x = 15.5
+
+#Veri tipi complex =>Örnek
+x = 1j
+
+#Veri tipi list =>Örnek
+x = ["berkay","engin","halit"]
+
+#Veri tipi tuple =>Örnek
+x = ("berkay","engin","halit")
+
+#Veri tipi range =>Örnek
+x = range(6)
+
+#Veri tipi dict =>Örnek
+x = {"isim" : "Berkay", "yas" : 20}
+
+#Veri tipi set =>Örnek
+x = {"kivi", "portakal", "ananas"}
+
+#Veri tipi frozenset =>Örnek
+x = frozenset({"kivi", "portakal", "ananas"})
+
+#Veri tipi bool =>Örnek
+x = True
+
+#Veri tipi bytes =>Örnek
+x = b"Hello"
+
+#Veri tipi bytearray =>Örnek
+x = bytearray(3)
+
+#Veri tipi memoryview =>Örnek
+x = memoryview(bytes(4))
+
+
+#Soru 2 
+# kodlamaio sitesinde Kurslarım, Tüm Kurslar, Kariyer, Sık Sorulanlar string veri tipindedir.
+# Kategori ve eğitmen içerikleri list veri tipidir
+# eğitimi %50 tamamladı demesi integer veri tipidir
+
+
+#Soru 3 
+bitirVeDevam = True
+# Eğer bitirmişseniz lütfen bitir ve devam et butonuna tıklayınız.
+
+if bitirVeDevam == True:
+    print("Dairenin içinde tik işareti olsun")
+
+else:
+    print("Dairenin içi yarım dolu olsun")
+    
+       
+ 
+
+
